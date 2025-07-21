733dc74c0e437c908a27837c456fe139f91f433c
github-action-benchmark <github@users.noreply.github.com>
2025-07-21 16:33:17 +0000

    add Benchmark Results (julia) benchmark result for c11a566c9035cbfd7571e65ab87c62f5227d677b

--- benchmarks/data.js
+++ benchmarks/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1753114429363,
+  "lastUpdate": 1753115597008,
   "repoUrl": "https://github.com/oameye/FloquetExpansions.jl",
   "entries": {
     "Benchmark Results": [
@@ -40,6 +40,48 @@ window.BENCHMARK_DATA = {
           {
             "name": "Floquet Expansion/Rotating Wave Approximation/Kerr resonator",
             "value": 136342818,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=28303008\nallocs=691192\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "committer": {
+            "email": "orjan.ameye@hotmail.com",
+            "name": "Orjan Ameye",
+            "username": "oameye"
+          },
+          "distinct": true,
+          "id": "c11a566c9035cbfd7571e65ab87c62f5227d677b",
+          "message": "docs: add kerr resonator example",
+          "timestamp": "2025-07-21T18:31:03+02:00",
+          "tree_id": "cad520f3dd818dac0dc44da37e44f5fa72330c8b",
+          "url": "https://github.com/oameye/FloquetExpansions.jl/commit/c11a566c9035cbfd7571e65ab87c62f5227d677b"
+        },
+        "date": 1753115595983,
+        "tool": "julia",
+        "benches": [
+          {
+            "name": "Construction/Quasienergy operator/Kerr resonator",
+            "value": 101658,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=59616\nallocs=1171\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Operation/Rotate/Kerr resonator",
+            "value": 131188390,
+            "unit": "ns",
+            "extra": "gctime=0\nmemory=28243392\nallocs=690021\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
+          },
+          {
+            "name": "Floquet Expansion/Rotating Wave Approximation/Kerr resonator",
+            "value": 131204444,
             "unit": "ns",
             "extra": "gctime=0\nmemory=28303008\nallocs=691192\nparams={\"gctrial\":true,\"time_tolerance\":0.05,\"evals_set\":false,\"samples\":10000,\"evals\":1,\"gcsample\":false,\"seconds\":10,\"overhead\":0,\"memory_tolerance\":0.01}"
           }
