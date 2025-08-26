103acb9dd795dd67eb93000114b08eed570804e7
Orjan Ameye <orjan.ameye@hotmail.com>
2025-08-26 07:48:29 +0200

    add examplez

--- examples/kerr_resonator.jl
+++ examples/kerr_resonator.jl
@@ -1,9 +1,31 @@
-using FloquetExpansions
+# using FloquetExpansions
 
-h = FockSpace(:cavity)
-@rnumbers Δ F κ ω ω₀ α t
-@qnumbers a::Destroy(h)
+# h = FockSpace(:cavity)
+# @rnumbers Δ F κ ω ω₀ α t
+# @qnumbers a::Destroy(h)
 
-Ht = ω₀ * a' * a + α * (a' + a)^4 / 4 + F * (a' + a) * cos(ω * t)
+# Ht = ω₀ * a' * a + α * (a' + a)^4 / 4 + F * (a' + a) * cos(ω * t)
 
-rotating_wave_approximation(Ht, a, ω, t)
+# rotating_wave_approximation(Ht, a, ω, t)
+
+# #
+
+# Q = quasienergy_operator(Ht, a, ω, t)
+
+
+using VanVleckRecursion
+
+H = Terms([
+    Term(; rotating=0),    # H₀ (static part)
+    Term(; rotating=1),     # H₁cos(ωt) (oscillating part)
+    # Term(; rotating=2),     # H₁cos(ωt) (oscillating part)
+])
+
+set_hamiltonian!(H)
+
+# K(2)
+# term= K(2)
+# latex(term)
+
+term= K(3).terms[end]
+latex(term)
