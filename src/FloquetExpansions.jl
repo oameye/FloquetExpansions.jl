eb7db233b701cff1d9725a921f28b7395c068c6d
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-03 12:37:23 +0200

    first iteration theory

--- src/FloquetExpansions.jl
+++ src/FloquetExpansions.jl
@@ -20,6 +20,7 @@ include("engine.jl")
 export PeriodicGenerator, Gauge, VanVleck, QuasienergyOperator, harmonic_range
 export time_average, derivative, antiderivative, support, harmonics
 export FloquetExpansion, floquet_expansion, effective_generator, micromotion
-export Liouvillian, liouvillian, terms, hamiltonian_action, dissipator, compose, collapse, jump
+export Liouvillian,
+  liouvillian, terms, hamiltonian_action, dissipator, compose, collapse, jump
 
 end # module FloquetExpansions
