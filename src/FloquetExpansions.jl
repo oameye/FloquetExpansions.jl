a77533109f75079906fef637974d13bd3e56693b
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-04 19:52:33 +0200

    format GKSL exports

--- src/FloquetExpansions.jl
+++ src/FloquetExpansions.jl
@@ -24,6 +24,7 @@ export time_average, derivative, antiderivative, support, harmonics
 export FloquetExpansion, floquet_expansion, effective_generator, micromotion
 export Liouvillian,
   liouvillian, terms, hamiltonian_action, dissipator, compose, collapse, jump
-export DissipativeFrame, hamiltonian, hamiltonian_component, kossakowski, kossakowski_component
+export DissipativeFrame,
+  hamiltonian, hamiltonian_component, kossakowski, kossakowski_component
 
 end # module FloquetExpansions
