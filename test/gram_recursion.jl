9d69ccdbbb9fdb2bfea9818b109e97e5ff02f9cf
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-07 12:28:27 +0200

    feat: harden recursive Gram completion

---
+++ test/gram_recursion.jl
@@ -0,0 +1,51 @@
+using Test
+using FloquetExpansions
+using SecondQuantizedAlgebra: SecondQuantizedAlgebra
+using Symbolics: @variables
+
+const SQA = SecondQuantizedAlgebra
+
+@variables ω::Real t::Real Ω::Real
+
+@testset "transverse drive opens a second-order dephasing channel" begin
+  pauli = PauliSpace(:gram_recursive_pauli)
+  σx = Pauli(pauli, :sigma, 1)
+  σy = Pauli(pauli, :sigma, 2)
+  σz = Pauli(pauli, :sigma, 3)
+  frame = DissipativeFrame(σx, σy, σz)
+
+  H = Ω * cos(ω * t) * σx
+  expansion = floquet_expansion(H, ω, t, VanVleck(), 3; channels=(collapse(σz),))
+  completion = positive_completion(expansion, Gram(), frame)
+  gram = factorization(completion)
+
+  @test gram.onsets == [0, 1]
+  @test length(gram.stages) >= 2
+  @test gram.stages[1].grade == 0
+  @test gram.stages[2].grade == 2
+  @test size(gram.amplitudes[1], 2) == 2
+  @test all(iszero, gram.amplitudes[1][:, 2])
+  @test any(!iszero(value) for value in gram.amplitudes[2][:, 2])
+  @test liouvillian(hamiltonian(completion); channels=channels(completion)) ==
+    effective_generator(completion)
+end
+
+@testset "recursive filtration resolves nested even dark onsets" begin
+  N = 4
+  series = [FloquetExpansions.completion_matrix_zeros(3, 3) for _ in 0:N]
+  series[1][1, 1] = FloquetExpansions.completion_one()
+  series[3][2, 2] = FloquetExpansions.completion_one()
+  series[5][3, 3] = FloquetExpansions.completion_one()
+
+  conditions = FloquetExpansions.CompletionConditions()
+  factor, stages = FloquetExpansions.recursive_gram_factor_series(series, N, conditions)
+
+  @test [stage.grade for stage in stages] == [0, 2, 4]
+  @test size(first(factor)) == (3, 3)
+  @test !FloquetExpansions.structurally_zero(factor[1][1, 1])
+  @test all(FloquetExpansions.structurally_zero, factor[1][:, 2])
+  @test !all(FloquetExpansions.structurally_zero, factor[2][:, 2])
+  @test all(FloquetExpansions.structurally_zero, factor[1][:, 3])
+  @test all(FloquetExpansions.structurally_zero, factor[2][:, 3])
+  @test !all(FloquetExpansions.structurally_zero, factor[3][:, 3])
+end
