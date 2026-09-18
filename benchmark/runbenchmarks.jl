0f368c1fff3b988e3cab7428e8855014d1a9cbd0
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-18 09:42:47 +0200

    perf: register Bloch canonical reconstruction benchmark

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -4,9 +4,13 @@ using FloquetExpansions
 const SUITE = BenchmarkGroup()
 
 include("workflows.jl")
+include("bloch_van_vleck.jl")
+
 benchmark_fourier_expansion!(SUITE)
 benchmark_floquet_expansion!(SUITE)
 benchmark_positive_completion!(SUITE)
+benchmark_bloch_van_vleck!(SUITE)
+print_bvv_profiles()
 
 BenchmarkTools.tune!(SUITE)
 results = BenchmarkTools.run(SUITE; verbose=true)
