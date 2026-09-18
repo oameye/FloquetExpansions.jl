aef533c6349fce340414480a210c7d826ee5566b
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-18 11:19:46 +0200

    style: format connected-word certification

--- test/connected_word_log.jl
+++ test/connected_word_log.jl
@@ -5,29 +5,24 @@ include(joinpath(@__DIR__, "helpers", "connected_word_log.jl"))
 
 function cwl_compare_embeddings(left, right, zero_component)
   harmonics = union(keys(left), keys(right))
-  return all(get(left, harmonic, zero_component) == get(right, harmonic, zero_component) for harmonic in harmonics)
+  return all(
+    get(left, harmonic, zero_component) == get(right, harmonic, zero_component) for
+    harmonic in harmonics
+  )
 end
 
 @testset "formal harmonic-word reconstruction matches dense noncommuting algebra" begin
   R = Rational{Int}
   support = [-1, 0, 1]
-  components = Dict(
-    -1 => R[0 1; 2 -1],
-    0 => R[1 2; -1 0],
-    1 => R[2 -1; 1 1],
-  )
+  components = Dict(-1 => R[0 1; 2 -1], 0 => R[1 2; -1 0], 1 => R[2 -1; 1 1])
   zero_component = zeros(R, 2, 2)
   order = 6
   plan = FloquetExpansions.compile_bloch_projection_plan(support, order)
   direct_bloch = FloquetExpansions.evaluate_bloch_projection_plan(
-    plan,
-    components;
-    product=*,
-    inverse_weight=harmonic -> 1 // harmonic,
-    zero_component,
+    plan, components; product=(*), inverse_weight=harmonic -> 1 // harmonic, zero_component
   )
   direct = FloquetExpansions.bloch_van_vleck_reconstruction(
-    plan, direct_bloch; product=*, zero_component
+    plan, direct_bloch; product=(*), zero_component
   )
 
   word_plan, word_bloch, word = connected_word_reconstruction(support, order)
@@ -36,14 +31,14 @@ end
 
   for n in eachindex(word.effective)
     evaluated = evaluate_harmonic_word_polynomial(
-      word.effective[n], components; product=*, zero_component
+      word.effective[n], components; product=(*), zero_component
     )
     @test evaluated == direct.effective[n]
   end
 
   for n in eachindex(word.log_embedding)
     evaluated = evaluate_harmonic_word_embedding(
-      word.log_embedding[n], components; product=*, zero_component
+      word.log_embedding[n], components; product=(*), zero_component
     )
     @test cwl_compare_embeddings(evaluated, direct.log_embedding[n], zero_component)
   end
