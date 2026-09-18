c16074d46edc3a4bd6972d1cc62d582a7ddd49ed
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-18 11:50:22 +0200

    perf: register connected-log benchmarks

--- benchmark/runbenchmarks.jl
+++ benchmark/runbenchmarks.jl
@@ -4,9 +4,11 @@ using FloquetExpansions
 const SUITE = BenchmarkGroup()
 
 include("workflows.jl")
+include("lyndon_log_evaluation.jl")
 benchmark_fourier_expansion!(SUITE)
 benchmark_floquet_expansion!(SUITE)
 benchmark_positive_completion!(SUITE)
+benchmark_lyndon_log_evaluation!(SUITE)
 
 BenchmarkTools.tune!(SUITE)
 results = BenchmarkTools.run(SUITE; verbose=true)
