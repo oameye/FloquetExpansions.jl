e6586241a16b8b109a405dcc627a3d79fd722949
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-19 21:10:09 +0200

    Apply JuliaFormatter to Bloch recurrence JET gate

--- test/quality/JET.jl
+++ test/quality/JET.jl
@@ -9,13 +9,7 @@ end
 
 @testset "Bloch order recurrence optimizer stability" begin
   JET.@test_opt target_modules=(FloquetExpansions,) FloquetExpansions.evaluate_bloch_order_recurrence(
-    [1.0, 0.25],
-    5,
-    1.0,
-    0.0;
-    product=*,
-    project_model=identity,
-    solve_complement=identity,
+    [1.0, 0.25], 5, 1.0, 0.0; product=(*), project_model=identity, solve_complement=identity
   )
 end
 
