362e1632f571ada9f89171d63440281f3f41bc0f
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-19 08:40:37 +0200

    style: format cubic open-leg normalization oracle

--- test/helpers/open_leg_bloch_normalization.jl
+++ test/helpers/open_leg_bloch_normalization.jl
@@ -4,7 +4,8 @@ function openleg_bloch_static_factor2(bloch::OpenLegBlochReference; product)
 end
 
 function openleg_bloch_canonical_effective3(bloch::OpenLegBlochReference; product)
-  length(bloch.effective) >= 3 || throw(ArgumentError("third-order Bloch data are required"))
+  length(bloch.effective) >= 3 ||
+    throw(ArgumentError("third-order Bloch data are required"))
   static2 = openleg_bloch_static_factor2(bloch; product)
   correction = openleg_commutator(bloch.effective[1], static2, product)
   return bloch.effective[3] + correction
