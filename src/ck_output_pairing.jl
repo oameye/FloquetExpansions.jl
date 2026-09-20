cc5e587b283475b83357d316ff15c41b8166816b
Orjan Ameye <orjan.ameye@hotmail.com>
2026-09-20 11:19:51 +0200

    feat: grade CK pairing by Floquet phase

--- src/ck_output_pairing.jl
+++ src/ck_output_pairing.jl
@@ -1,6 +1,30 @@
-struct CKOutputPairingPolynomial{T}
-  terms::Dict{Int,T}
+struct CKOutputPairingGrade{H}
+  phase_harmonic::H
+  period_power::Int
+end
+
+function Base.isequal(left::CKOutputPairingGrade, right::CKOutputPairingGrade)
+  return isequal(left.phase_harmonic, right.phase_harmonic) &&
+         left.period_power == right.period_power
+end
+
+Base.:(==)(left::CKOutputPairingGrade, right::CKOutputPairingGrade) = isequal(left, right)
+
+function Base.hash(grade::CKOutputPairingGrade, seed::UInt)
+  return hash(grade.period_power, hash(grade.phase_harmonic, seed))
+end
+
+struct CKOutputPairingPolynomial{H,T}
+  terms::Dict{CKOutputPairingGrade{H},T}
   zero_component::T
+end
+
+function CKOutputPairingPolynomial(terms::Dict{Int,T}, zero_component::T) where {T}
+  graded_terms = Dict{CKOutputPairingGrade{Int},T}()
+  for (period_power, value) in terms
+    graded_terms[CKOutputPairingGrade(0, period_power)] = value
+  end
+  return CKOutputPairingPolynomial(graded_terms, zero_component)
 end
 
 function Base.isequal(left::CKOutputPairingPolynomial, right::CKOutputPairingPolynomial)
@@ -13,16 +37,34 @@ function Base.:(==)(left::CKOutputPairingPolynomial, right::CKOutputPairingPolyn
 end
 
 function ck_output_pairing_accumulate!(
-  terms::Dict{Int,T}, period_power::Int, value::T, zero_component::T
-) where {T}
-  updated = get(terms, period_power, zero_component) + value
+  terms::Dict{CKOutputPairingGrade{H},T},
+  grade::CKOutputPairingGrade{H},
+  value::T,
+  zero_component::T,
+) where {H,T}
+  updated = get(terms, grade, zero_component) + value
   if ck_kernel_iszero(updated)
-    haskey(terms, period_power) && delete!(terms, period_power)
+    haskey(terms, grade) && delete!(terms, grade)
   else
-    terms[period_power] = updated
+    terms[grade] = updated
   end
   return terms
 end
+
+function ck_output_pairing_accumulate!(
+  terms::Dict{CKOutputPairingGrade{H},T},
+  phase_harmonic::H,
+  period_power::Int,
+  value::T,
+  zero_component::T,
+) where {H,T}
+  return ck_output_pairing_accumulate!(
+    terms, CKOutputPairingGrade(phase_harmonic, period_power), value, zero_component
+  )
+end
+
+ck_output_channel_phase(left, right) = left - right
+ck_output_metric_phase(left, right) = right - left
 
 function ck_output_period_pairing(
   left::CKOutputPeriodPolynomial{H,S},
@@ -31,8 +73,9 @@ function ck_output_period_pairing(
   zero_component::R,
   gram_weight,
   system_pair,
+  phase_pair,
 ) where {H<:Integer,S,T,R}
-  result = Dict{Int,R}()
+  result = Dict{CKOutputPairingGrade{H},R}()
 
   for (left_index, left_kernel) in enumerate(left.coefficients),
     (right_index, right_kernel) in enumerate(right.coefficients)
@@ -43,16 +86,19 @@ function ck_output_period_pairing(
     for (left_key, left_value) in left_kernel.terms,
       (right_key, right_value) in right_kernel.terms
 
-      left_key.output_channels == right_key.output_channels || continue
+      isequal(left_key.output_channels, right_key.output_channels) || continue
       output_number = length(left_key.output_channels)
       inverse_fourier_shift = left_period_power + right_period_power - 2 * output_number
       overlap = ck_output_time_overlap(left_key, right_key, imaginary)
       paired_system = system_pair(left_value, right_value)
+      phase_harmonic = phase_pair(left_key.phase_harmonic, right_key.phase_harmonic)
 
       for (overlap_period_power, overlap_value) in overlap.terms
         total_period_power = inverse_fourier_shift + overlap_period_power
         value = gram_weight(overlap_value) * paired_system
-        ck_output_pairing_accumulate!(result, total_period_power, value, zero_component)
+        ck_output_pairing_accumulate!(
+          result, phase_harmonic, total_period_power, value, zero_component
+        )
       end
     end
   end
@@ -67,7 +113,13 @@ function ck_output_channel_pairing(
   channel_pair,
 ) where {H<:Integer,S,T,R}
   return ck_output_period_pairing(
-    left, right, imaginary, zero_component, identity, channel_pair
+    left,
+    right,
+    imaginary,
+    zero_component,
+    identity,
+    channel_pair,
+    ck_output_channel_phase,
   )
 end
 
@@ -78,15 +130,44 @@ function ck_output_metric_pairing(
   zero_component::R,
   metric_pair,
 ) where {H<:Integer,S,T,R}
-  return ck_output_period_pairing(left, right, imaginary, zero_component, conj, metric_pair)
+  return ck_output_period_pairing(
+    left,
+    right,
+    imaginary,
+    zero_component,
+    conj,
+    metric_pair,
+    ck_output_metric_phase,
+  )
 end
 
 function ck_output_pairing_coefficient(
-  polynomial::CKOutputPairingPolynomial{T}, period_power::Int
-) where {T}
-  return get(polynomial.terms, period_power, polynomial.zero_component)
+  polynomial::CKOutputPairingPolynomial{H,T}, phase_harmonic::H, period_power::Int
+) where {H,T}
+  grade = CKOutputPairingGrade(phase_harmonic, period_power)
+  return get(polynomial.terms, grade, polynomial.zero_component)
+end
+
+function ck_output_pairing_coefficient(
+  polynomial::CKOutputPairingPolynomial{H,T}, period_power::Int
+) where {H,T}
+  result = polynomial.zero_component
+  for (grade, value) in polynomial.terms
+    grade.period_power == period_power || continue
+    result += value
+  end
+  return result
+end
+
+function ck_output_pairing_phase_support(polynomial::CKOutputPairingPolynomial{H}) where {H}
+  phases = H[]
+  for grade in keys(polynomial.terms)
+    grade.phase_harmonic in phases || push!(phases, grade.phase_harmonic)
+  end
+  sort!(phases)
+  return phases
 end
 
 function ck_output_pairing_has_negative_power(polynomial::CKOutputPairingPolynomial)
-  return any(period_power -> period_power < 0, keys(polynomial.terms))
+  return any(grade -> grade.period_power < 0, keys(polynomial.terms))
 end
